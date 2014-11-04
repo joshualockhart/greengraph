6ed75955757b5ab803c90da47e356c309bc63000
joshualockhart <joshualockhart@gmail.com>
2014-11-04 19:01:47 +0000

    made the greengraph script runnable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "GreenGraph",
-    version = "0.2",
+    version = "0.2.1",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
     install_requires = ['argparse', 'pypng', 'geopy', 'requests']
